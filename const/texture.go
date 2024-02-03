c4c89fcf621eb50278ff2ba8a09f3b8a44de8d25
TrashPony <TrashPony13@gmail.com>
2024-02-03 17:32:54 +0300

    ++++++

--- const/texture.go
+++ const/texture.go
@@ -5,7 +5,7 @@ func GetTextureKostil(texture, objectType string) (bool, bool) {
 	if !(texture == "replic_gauss_gun" || texture == "unknown_civilization_jammer" ||
 		texture == "explores_antenna" || texture == "explores_observatory" || texture == "mini_turret_1" ||
 		texture == "mini_turret_2" || texture == "mini_shield_generator_1" || texture == "corporation_base_1" ||
-		texture == "corporation_base_1_build") {
+		texture == "corporation_base_1_build" || texture == "corporation_base_1_destroy") {
 
 		if objectType == "turret" || objectType == "shield" || objectType == "generator" ||
 			objectType == "jammer" || objectType == "missile_defense" || objectType == "meteorite_defense" ||
