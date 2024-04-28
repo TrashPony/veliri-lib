3cfdaefb10799ec9465f0eac83993690b6ba4cca
TrashPony <TrashPony13@gmail.com>
2024-04-28 19:08:48 +0300

    ++

--- handbook/interface.go
+++ handbook/interface.go
@@ -2205,7 +2205,37 @@ class='importantly'>%credits_count% cr</span> from the main deposit?`,
 			_const.EN: `Buy`,
 		},
 	},
-	"FriendList": {},
+	"FriendList": {
+		"window_name": {
+			_const.RU: `Контакты`,
+			_const.EN: `Contacts`,
+		},
+		"tab_1": {
+			_const.RU: `Друзья`,
+			_const.EN: `Friends`,
+		},
+		"tab_2": {
+			_const.RU: `Контакты`,
+			_const.EN: `Contacts`,
+		},
+		"tab_3": {
+			_const.RU: `Заблокированные`,
+			_const.EN: `Blocked`,
+		},
+		"placeholder_1": {
+			_const.RU: `Имя игрока`,
+			_const.EN: `Username`,
+		},
+		"button_1": {
+			_const.RU: `Отправить запрос`,
+			_const.EN: `Send request`,
+		},
+		"button_2": {
+			_const.RU: `Добавить контакт`,
+			_const.EN: `Add contact`,
+		},
+	},
+	"Game": {},
 	"Gate": {
 		"window_name": {
 			_const.RU: `Вход`,
