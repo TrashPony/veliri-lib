e51c0082f1e341899b5af6059bc74f4c1f782d5d
TrashPony <TrashPony13@gmail.com>
2024-04-28 19:46:19 +0300

    ++

--- handbook/interface.go
+++ handbook/interface.go
@@ -2372,7 +2372,34 @@ class='importantly'>%credits_count% cr</span> from the main deposit?`,
 			_const.EN: `You are the enemy in this sector!`,
 		},
 	},
-	"GlobalMap":         {},
+	"GlobalMap": {
+		// GlobalMap
+		"window_name": {
+			_const.RU: `Карта мира`,
+			_const.EN: `World map`,
+		},
+		// MapPin
+		"button_1": {
+			_const.RU: `Автопилот`,
+			_const.EN: `Autopilot`,
+		},
+		"button_2": {
+			_const.RU: `Установить цель`,
+			_const.EN: `Set a path`,
+		},
+		"text_1": {
+			_const.RU: `В секторе идет бой!`,
+			_const.EN: `There is a battle in the sector!`,
+		},
+		"text_2": {
+			_const.RU: `Атака возможна:`,
+			_const.EN: `Attack possible:`,
+		},
+		"button_3": {
+			_const.RU: `Закрыть`,
+			_const.EN: `Close`,
+		},
+	},
 	"GroupMenu":         {},
 	"Hangar":            {},
 	"Help":              {},
