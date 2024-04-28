ffb04b82266638ce52553b44f4e538ec05a142f7
TrashPony <TrashPony13@gmail.com>
2024-04-28 18:57:08 +0300

    ++

--- handbook/interface.go
+++ handbook/interface.go
@@ -2142,9 +2142,70 @@ class='importantly'>%credits_count% cr</span> from the main deposit?`,
 			_const.EN: `Skills`,
 		},
 	},
-	"EquipBar":       {},
-	"FractionMarket": {},
-	"FriendList":     {},
+	"EquipBar": {},
+	"FractionMarket": {
+		"window_name": {
+			_const.RU: `Фракционный магазин`,
+			_const.EN: `Faction Store`,
+		},
+		"text_1": {
+			_const.RU: `Фракционный магазин`,
+			_const.EN: `Faction Store`,
+		},
+		"text_2": {
+			_const.RU: `Кол-во очков фракции`,
+			_const.EN: `Faction Points`,
+		},
+		"text_3": {
+			_const.RU: `оф.`,
+			_const.EN: `p.`,
+		},
+		"text_4": {
+			_const.RU: `Магазин доступен только членам ополчения.`,
+			_const.EN: `The store is only available to militia members.`,
+		},
+		"text_5": {
+			_const.RU: `Предмет`,
+			_const.EN: `Item`,
+		},
+		"text_6": {
+			_const.RU: `Кол-во`,
+			_const.EN: `Qty.`,
+		},
+		"text_7": {
+			_const.RU: `Необходимый ранг`,
+			_const.EN: `Required rank`,
+		},
+		"text_8": {
+			_const.RU: `Стоимость`,
+			_const.EN: `Price`,
+		},
+		"button_1": {
+			_const.RU: `Купить`,
+			_const.EN: `Buy`,
+		},
+		"text_9": {
+			_const.RU: `Приобрести`,
+			_const.EN: `Price`,
+		},
+		"text_10": {
+			_const.RU: `за`,
+			_const.EN: `for`,
+		},
+		"text_11": {
+			_const.RU: `и`,
+			_const.EN: `and`,
+		},
+		"button_2": {
+			_const.RU: `Отмена`,
+			_const.EN: `Cancel`,
+		},
+		"button_3": {
+			_const.RU: `Купить`,
+			_const.EN: `Buy`,
+		},
+	},
+	"FriendList": {},
 	"Gate": {
 		"window_name": {
 			_const.RU: `Вход`,
