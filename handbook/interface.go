d23d38b45106117c01ee9ecf2bf05d7770e3995e
TrashPony <TrashPony13@gmail.com>
2024-05-06 19:40:17 +0300

    ++

--- handbook/interface.go
+++ handbook/interface.go
@@ -3556,12 +3556,328 @@ class='importantly'>%credits_count% cr</span> from the main deposit?`,
 			_const.EN: `Back`,
 		},
 	},
-	"MenuBar":           {},
-	"MiniGames":         {},
-	"MiniMap":           {},
-	"Missions":          {},
-	"ModalNotify":       {},
-	"News":              {},
+	"MenuBar": {
+		// MenuBar
+		"text_1": {
+			_const.RU: `Общение`,
+			_const.EN: `Communication`,
+		},
+		"sub_1": {
+			_const.RU: `Личное дело`,
+			_const.EN: `Personal information`,
+		},
+		"sub_2": {
+			_const.RU: `Контакты`,
+			_const.EN: `Contacts`,
+		},
+		"sub_3": {
+			_const.RU: `Группа`,
+			_const.EN: `Group`,
+		},
+		"sub_4": {
+			_const.RU: `Кластер`,
+			_const.EN: `Corporation`,
+		},
+		"text_2": {
+			_const.RU: `Помощь`,
+			_const.EN: `Help`,
+		},
+		"sub_5": {
+			_const.RU: `Управление`,
+			_const.EN: `Control`,
+		},
+		"sub_6": {
+			_const.RU: `Помощь`,
+			_const.EN: `Help`,
+		},
+		"sub_7": {
+			_const.RU: `Технологии`,
+			_const.EN: `Technologies`,
+		},
+		"sub_8": {
+			_const.RU: `Группа ВК`,
+			_const.EN: `VK group`,
+		},
+		"sub_9": {
+			_const.RU: `Discord`,
+			_const.EN: `Discord`,
+		},
+		"text_3": {
+			_const.RU: `Автопилот`,
+			_const.EN: `Autopilot`,
+		},
+		"text_4": {
+			_const.RU: `Эвакуация`,
+			_const.EN: `Evacuation`,
+		},
+		"text_5": {
+			_const.RU: `Топливо`,
+			_const.EN: `Fuel`,
+		},
+		// Notifys
+	},
+	"MiniGames": {
+		"window_name": {
+			_const.RU: `Мини-игра`,
+			_const.EN: `Mini-game`,
+		},
+		"text_1": {
+			_const.RU: `Победа!`,
+			_const.EN: `Win!`,
+		},
+		"button_1": {
+			_const.RU: `Завершить`,
+			_const.EN: `Complete`,
+		},
+	},
+	"MiniMap": {
+		"zoom": {
+			_const.RU: `Зум`,
+			_const.EN: `Zoom`,
+		},
+		"fix_cam": {
+			_const.RU: `Фиксация камеры`,
+			_const.EN: `Camera fixation`,
+		},
+		"battle_cam": {
+			_const.RU: `Боевой обзор`,
+			_const.EN: `Combat review`,
+		},
+	},
+	"Missions": {
+		// Missions
+		"window_name": {
+			_const.RU: `Активные задания`,
+			_const.EN: `Active tasks`,
+		},
+		"button_1": {
+			_const.RU: `задания`,
+			_const.EN: `missions`,
+		},
+		// Points
+		"text_1": {
+			_const.RU: `Обновлено`,
+			_const.EN: `Updated`,
+		},
+		"button_2": {
+			_const.RU: `помощь`,
+			_const.EN: `help`,
+		},
+		"button_3": {
+			_const.RU: `потерял предмет?`,
+			_const.EN: `lost an item?`,
+		},
+	},
+	"ModalNotify": {
+		"window_name": {
+			_const.RU: `Наблюдение`,
+			_const.EN: `Observation`,
+		},
+	},
+	"News": {
+		// News
+		"window_name": {
+			_const.RU: `Новости`,
+			_const.EN: `News`,
+		},
+		"tab_1": {
+			_const.RU: `Новости`,
+			_const.EN: `News`,
+		},
+		"tab_2": {
+			_const.RU: `Поиск`,
+			_const.EN: `Search`,
+		},
+		"n_tab_1": {
+			_const.RU: `Все`,
+			_const.EN: `Everything`,
+		},
+		"n_tab_2": {
+			_const.RU: `Торговля`,
+			_const.EN: `Trade`,
+		},
+		"n_tab_3": {
+			_const.RU: `Война`,
+			_const.EN: `War`,
+		},
+		"n_tab_4": {
+			_const.RU: `Экспедиция`,
+			_const.EN: `Expedition`,
+		},
+		"n_tab_5": {
+			_const.RU: `Аномалии`,
+			_const.EN: `Anomalies`,
+		},
+		"search_t_1": {
+			_const.RU: `Персона`,
+			_const.EN: `Persona`,
+		},
+		"search_t_2": {
+			_const.RU: `База`,
+			_const.EN: `Base`,
+		},
+		"search_t_3": {
+			_const.RU: `Товары`,
+			_const.EN: `Product`,
+		},
+		"button_1": {
+			_const.RU: `Найти (0.3 cr.)`,
+			_const.EN: `Find (0.3 cr.)`,
+		},
+		"base_t_1": {
+			_const.RU: `Перерабатывающий завод`,
+			_const.EN: `Processing plant`,
+		},
+		"base_t_2": {
+			_const.RU: `Основная база`,
+			_const.EN: `Main base`,
+		},
+		"base_t_3": {
+			_const.RU: `Завод расходных материалов`,
+			_const.EN: `Factory`,
+		},
+		"range_1": {
+			_const.RU: `Налог до`,
+			_const.EN: `Tax up to`,
+		},
+		"range_2": {
+			_const.RU: `Расстояние до`,
+			_const.EN: `Distance to`,
+		},
+		"text_1": {
+			_const.RU: `Кол-во:`,
+			_const.EN: `Qty:`,
+		},
+		"text_2": {
+			_const.RU: `Макс. цена продажи:`,
+			_const.EN: `Max selling price:`,
+		},
+		"text_3": {
+			_const.RU: `Мин. цена покупки:`,
+			_const.EN: `Min. purchase price:`,
+		},
+		"nothing": {
+			_const.RU: `Ничего не найдено`,
+			_const.EN: `Nothing found`,
+		},
+		"target_1": {
+			_const.RU: `Регион:`,
+			_const.EN: `Region:`,
+		},
+		"target_2": {
+			_const.RU: `Сектор:`,
+			_const.EN: `Sector:`,
+		},
+		"target_3": {
+			_const.RU: `База:`,
+			_const.EN: `Base:`,
+		},
+		"button_2": {
+			_const.RU: `карта`,
+			_const.EN: `map`,
+		},
+		"text_4": {
+			_const.RU: `Налог:`,
+			_const.EN: `Tax:`,
+		},
+		"text_5": {
+			_const.RU: `расстояние:`,
+			_const.EN: `distance:`,
+		},
+		"text_6": {
+			_const.RU: `Расстояние:`,
+			_const.EN: `Distance:`,
+		},
+		"text_7": {
+			_const.RU: `кол-во:`,
+			_const.EN: `qty:`,
+		},
+		"text_8": {
+			_const.RU: `Покупка:`,
+			_const.EN: `Purchase:`,
+		},
+		"text_9": {
+			_const.RU: `Продажа:`,
+			_const.EN: `Sale:`,
+		},
+		// Notify
+		"text_10": {
+			_const.RU: `Завершено производство`,
+			_const.EN: `Production completed`,
+		},
+		"on_base": {
+			_const.RU: `На базе`,
+			_const.EN: `On the base`,
+		},
+		"sector": {
+			_const.RU: `сектор:`,
+			_const.EN: `sector:`,
+		},
+		"text_11": {
+			_const.RU: `завершено производство:`,
+			_const.EN: `production completed:`,
+		},
+		"text_12": {
+			_const.RU: `Созданные вещи ожидают вас на складе базы.`,
+			_const.EN: `The created items are waiting for you in the base warehouse.`,
+		},
+		"text_13": {
+			_const.RU: `Совершена продажа`,
+			_const.EN: `Sale completed`,
+		},
+		"text_14": {
+			_const.RU: `Совершена покупка`,
+			_const.EN: `Purchase completed`,
+		},
+		"text_15": {
+			_const.RU: `было продано:`,
+			_const.EN: `sold:`,
+		},
+		"text_16": {
+			_const.RU: `было куплено:`,
+			_const.EN: `was purchased:`,
+		},
+		"text_17": {
+			_const.RU: `Вы заработали:`,
+			_const.EN: `You've earned:`,
+		},
+		"text_18": {
+			_const.RU: `Купленные вещи ожидают вас на складе базы.`,
+			_const.EN: `The purchased items are waiting for you at the base warehouse.`,
+		},
+		"dialog": {
+			_const.RU: `Диалог`,
+			_const.EN: `Dialog`,
+		},
+		"from": {
+			_const.RU: `От`,
+			_const.EN: `From`,
+		},
+		"to": {
+			_const.RU: `Кому`,
+			_const.EN: `To`,
+		},
+		"attention": {
+			_const.RU: `Внимание!`,
+			_const.EN: `Attention!`,
+		},
+		"request": {
+			_const.RU: `Запрос`,
+			_const.EN: `Request`,
+		},
+		"transfer": {
+			_const.RU: `Перевод`,
+			_const.EN: `Transfer`,
+		},
+		"relation_text": {
+			_const.RU: `Отношения с фракцией <span class="importantly">%fraction%</span> улучшены.`,
+			_const.EN: `Relations with the <span class="importantly">%fraction%</span> faction deteriorated.`,
+		},
+		"close": {
+			_const.RU: `Закрыть`,
+			_const.EN: `Close`,
+		},
+	},
 	"NPCTrade":          {},
 	"ObjectDialog":      {},
 	"PrefabricatedMenu": {},
