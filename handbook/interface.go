15b27762b5ed7bd2fc1c69ad257360bacaea17a8
TrashPony <TrashPony13@gmail.com>
2024-05-06 20:14:38 +0300

    ++

--- handbook/interface.go
+++ handbook/interface.go
@@ -3968,9 +3968,61 @@ class='importantly'>%credits_count% cr</span> from the main deposit?`,
 			_const.EN: `Resources needed`,
 		},
 	},
-	"Preloader":      {},
-	"ProcessorRoot":  {},
-	"SelectTarget":   {},
+	"Preloader": {},
+	"ProcessorRoot": {
+		// ItemsDropZone
+		"head": {
+			_const.RU: `Переработать:`,
+			_const.EN: `Recycler:`,
+		},
+		"text_1": {
+			_const.RU: `Перетащите сюда предметы которые хотите переработать.`,
+			_const.EN: `Drag the items you want to recycle here.`,
+		},
+		// ProcessorRoot
+		"window_name": {
+			_const.RU: `Переработчик`,
+			_const.EN: `Processor`,
+		},
+		"to_storage": {
+			_const.RU: `Предметы отправлены на <span class="importantly">склад</span>`,
+			_const.EN: `Items sent to <span class="importantly">warehouse</span>`,
+		},
+		"text_2": {
+			_const.RU: `Модуль сервиса уничтожен или отключен.`,
+			_const.EN: `The service module has been destroyed or disabled.`,
+		},
+		"not_allow": {
+			_const.RU: `Мы не доверяем вам и поэтому этот сервис не доступен.`,
+			_const.EN: `We do not trust you and therefore this service is not available.`,
+		},
+		"lost_items": {
+			_const.RU: `Потери:`,
+			_const.EN: `Losses:`,
+		},
+		"tax": {
+			_const.RU: `Налог:`,
+			_const.EN: `Tax:`,
+		},
+		"result": {
+			_const.RU: `Результат:`,
+			_const.EN: `Result:`,
+		},
+		"button_1": {
+			_const.RU: `Закрыть`,
+			_const.EN: `Close`,
+		},
+		"button_2": {
+			_const.RU: `Переработать`,
+			_const.EN: `Recycle`,
+		},
+	},
+	"SelectTarget": {
+		"title_1": {
+			_const.RU: `диалог`,
+			_const.EN: `dialog`,
+		},
+	},
 	"SendCredits":    {},
 	"ServerState":    {},
 	"Settings":       {},
