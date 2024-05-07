81fe63a84d1902d2bd1ec4a25fd2359d432ca777
TrashPony <TrashPony13@gmail.com>
2024-05-07 13:34:49 +0300

    ++

--- handbook/interface.go
+++ handbook/interface.go
@@ -4023,7 +4023,168 @@ class='importantly'>%credits_count% cr</span> from the main deposit?`,
 			_const.EN: `dialog`,
 		},
 	},
-	"SendCredits":    {},
+	"SendCredits": {
+		"window_name": {
+			_const.RU: `Кредиты`,
+			_const.EN: `Credits`,
+		},
+		"transfer": {
+			_const.RU: `Перевод`,
+			_const.EN: `Transfer`,
+		},
+		"history": {
+			_const.RU: `История`,
+			_const.EN: `History`,
+		},
+		"text_1": {
+			_const.RU: `Откуда:`,
+			_const.EN: `Src:`,
+		},
+		"text_2": {
+			_const.RU: `Куда:`,
+			_const.EN: `Dst:`,
+		},
+		"option_1": {
+			_const.RU: `Мой счет`,
+			_const.EN: `My deposit`,
+		},
+		"player": {
+			_const.RU: `Игрок`,
+			_const.EN: `Player`,
+		},
+		"cluster": {
+			_const.RU: `Кластер`,
+			_const.EN: `Cluster`,
+		},
+		"placeholder_1": {
+			_const.RU: `Имя`,
+			_const.EN: `Name`,
+		},
+		"button_1": {
+			_const.RU: `Найти`,
+			_const.EN: `Search`,
+		},
+		"nothing": {
+			_const.RU: `Ничего не найдено.`,
+			_const.EN: `Nothing found.`,
+		},
+		"text_3": {
+			_const.RU: `Сколько:`,
+			_const.EN: `How many:`,
+		},
+		"placeholder_2": {
+			_const.RU: `причина (необяз.)`,
+			_const.EN: `reason (optional)`,
+		},
+		"button_2": {
+			_const.RU: `Перевести`,
+			_const.EN: `Send`,
+		},
+		"text_4": {
+			_const.RU: `Операция`,
+			_const.EN: `Operation`,
+		},
+		"text_5": {
+			_const.RU: `Причина`,
+			_const.EN: `Cause`,
+		},
+		"on_1": {
+			_const.RU: `Перевод от кластера <span class="importantly">%name%</span>`,
+			_const.EN: `Transfer from cluster <span class="importantly">%name%</span>`,
+		},
+		"on_2": {
+			_const.RU: `Перевод кластеру <span class="importantly">%name%</span>`,
+			_const.EN: `Transfer to cluster <span class="importantly">%name%</span>`,
+		},
+		"on_3": {
+			_const.RU: `Перевод от <span class="importantly">%name%</span>`,
+			_const.EN: `Translation from <span class="importantly">%name%</span>`,
+		},
+		"on_4": {
+			_const.RU: `Перевод <span class="importantly">%name%</span>`,
+			_const.EN: `Translation <span class="importantly">%name%</span>`,
+		},
+		"on_5": {
+			_const.RU: `Налог с продажи`,
+			_const.EN: `Sales tax`,
+		},
+		"on_6": {
+			_const.RU: `Стартовый капитал`,
+			_const.EN: `Start-up capital`,
+		},
+		"on_7": {
+			_const.RU: `Продажа товара`,
+			_const.EN: `Sale of goods`,
+		},
+		"on_8": {
+			_const.RU: `Покупка товара`,
+			_const.EN: `Purchase of goods`,
+		},
+		"on_9": {
+			_const.RU: `Продажа на рынке`,
+			_const.EN: `Selling on the market`,
+		},
+		"on_10": {
+			_const.RU: `Покупка на рынке`,
+			_const.EN: `Purchase at the market`,
+		},
+		"on_11": {
+			_const.RU: `Размещение заказа`,
+			_const.EN: `Placing an order`,
+		},
+		"on_12": {
+			_const.RU: `Отмена заказа`,
+			_const.EN: `Cancellations`,
+		},
+		"on_13": {
+			_const.RU: `Выполнение миссии`,
+			_const.EN: `Completing the mission`,
+		},
+		"on_14": {
+			_const.RU: `Покупка покраски`,
+			_const.EN: `Buying skin`,
+		},
+		"on_15": {
+			_const.RU: `Грабеж`,
+			_const.EN: `Robbery`,
+		},
+		"on_16": {
+			_const.RU: `Починка транспорта`,
+			_const.EN: `Vehicle repair`,
+		},
+		"on_17": {
+			_const.RU: `Починка предмета`,
+			_const.EN: `Repairing an item`,
+		},
+		"on_18": {
+			_const.RU: `Покупка в фр. магазине`,
+			_const.EN: `Purchasing from a faction store`,
+		},
+		"on_19": {
+			_const.RU: `Поиск`,
+			_const.EN: `Search`,
+		},
+		"on_20": {
+			_const.RU: `Устранение преступника`,
+			_const.EN: `Elimination of the criminal`,
+		},
+		"on_21": {
+			_const.RU: `Устранение бойца в войне`,
+			_const.EN: `Eliminating a fighter in a war`,
+		},
+		"on_22": {
+			_const.RU: `Устранение сил APD`,
+			_const.EN: `Elimination of APD forces`,
+		},
+		"on_23": {
+			_const.RU: `Покупка боеприпасов`,
+			_const.EN: `Buying ammunition`,
+		},
+		"on_24": {
+			_const.RU: `Заправка транспорта`,
+			_const.EN: `Refueling vehicles`,
+		},
+	},
 	"ServerState":    {},
 	"Settings":       {},
 	"StatusBar":      {},
