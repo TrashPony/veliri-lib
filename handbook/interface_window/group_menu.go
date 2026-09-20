aac03d9f485401cd2e43d500964cd5de23fe78e1
TrashPony <TrashPony13@gmail.com>
2026-09-20 03:24:38 +0300

    ++

--- handbook/interface_window/group_menu.go
+++ handbook/interface_window/group_menu.go
@@ -13,4 +13,14 @@ var GroupMenu = map[string]map[string]string{
 		_const.EN:   `You are not a member of the squad...`,
 		_const.ZhCN: `你不是队伍的一员...`,
 	},
+	"party": {
+		_const.RU:   `Пати`,
+		_const.EN:   `Party`,
+		_const.ZhCN: `队伍`,
+	},
+	"team": {
+		_const.RU:   `Команда`,
+		_const.EN:   `Team`,
+		_const.ZhCN: `团队`,
+	},
 }
